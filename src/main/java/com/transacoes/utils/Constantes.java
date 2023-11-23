6fd4b5cb1d37303eb4db3df55bdc227ce0a81651
Julio Barros <juliocezarbarros2@gmail.com>
2023-11-22 23:27:50 -0300

    Criando classes de constantes

---
+++ src/main/java/com/transacoes/utils/Constantes.java
@@ -0,0 +1,9 @@
+package com.transacoes.utils;
+
+/**
+ * Classe com constantes
+ */
+public class Constantes {
+    
+    public static final String PARAMETRO_OBRIGATORIO = "Parâmetro obrigatório para busca";
+}
